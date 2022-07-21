742eb145eadf0fd47dc7852b611eb6f9c5f21c57
Azu0925 <yamaneko564783@gmail.com>
2022-07-21 18:38:01 +0900

    complete

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const checkBox = document.getElementById("check");
+  const text = document.getElementById("text");
+  if (checkBox.checked == true) {
+    text.style.backgroundColor = "red";
+  } else {
+    text.style.backgroundColor = "";
+  }
 }
